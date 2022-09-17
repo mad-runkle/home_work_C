7d6e7f9412acf082efeaa74e0ac0bcf538d66629
Andrey Agafonov <runkle@yandex.ru>
2022-09-17 22:02:55 +0400

    Added Seminar_1\Example_2

---
+++ Seminar_1/Examlpe_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else if (num1 == num2)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    else Console.WriteLine("Первое число меньше второго");
