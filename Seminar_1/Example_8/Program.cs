fac1580d2d6a3b3b7b968347dfb0d4689f221f50
Andrey Agafonov <runkle@yandex.ru>
2022-09-18 21:57:20 +0400

    Added Example_8

---
+++ Seminar_1/Example_8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+if (num > 0)
+{
+    while (evenNumber <= num)
+    {
+        Console.Write($"{evenNumber}, ");
+        evenNumber = evenNumber + 2;
+    }
+}
+else if (num == 0)
+    {
+        Console.WriteLine("Вы ввели 0");
+    }
+    else if (num < 0)
+        {
+            Console.WriteLine("Вы ввели отрицательное число");
+        }
