d35969d65a90012237904e506d71b099cec32b1d
Andrey Agafonov <runkle@yandex.ru>
2022-09-19 21:14:10 +0400

    Update Example_4

--- Seminar_1/Example_4/Program.cs
+++ Seminar_1/Example_4/Program.cs
@@ -7,12 +7,6 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
-if (num2 > max)
-{
-    max = num2;
-}
-if (num3 > max)
-{
-    max = num3;
-}
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 Console.WriteLine($"Максимальное число: {max}");
