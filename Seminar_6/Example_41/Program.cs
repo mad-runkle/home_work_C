359ea74d9f73a7c30a8c22f0eabc28d3a073a9b9
Andrey Agafonov <runkle@yandex.ru>
2022-10-08 00:34:22 +0400

    Added Homework Examples for Seminar_6

---
+++ Seminar_6/Example_41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Сколько чисел вы собираетесь ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int count = 0;
+FillArray(numbers);
+PositiveNumbersCount(numbers);
+Console.WriteLine("Положительных чисел введено: " + count);
+
+void FillArray(int[] numbers)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i+1} число: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PositiveNumbersCount(int[] numbers)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] > 0) count++;
+    }
+}
+
+
---
+++ Seminar_6/Example_43/Program.cs
@@ -0,0 +1,26 @@
+﻿/*  Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.
+    значения b1, k1, b2 и k2 задаются пользователем. */
+
+Console.WriteLine("Введите коэффициенты первой прямой");
+Console.Write("k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициенты второй прямой");
+Console.Write("k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 != k2)
+{
+    double pointX = (b2 - b1) / (k1 - k2);
+    double pointY = k1 * pointX + b1;
+    pointX = Math.Round(pointX, 1);
+    pointY = Math.Round(pointY, 1);
+    Console.WriteLine($"Прямые пересекаются в точке А({pointX}, {pointY})");
+}
+else
+{
+    Console.WriteLine("Прямые параллельны");
+}
